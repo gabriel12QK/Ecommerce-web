72286a4f365e868ca0defe747ae8f12f6fde6853
bermeo2000 <luismaciasantonio2000@gmail.com>
2022-12-18 20:44:10 -0500

    Consumo de Api

--- src/app/components/ingreso-producto/ingreso-producto.component.ts
+++ src/app/components/ingreso-producto/ingreso-producto.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
 import { Router } from '@angular/router'
-
+import { environment } from 'src/environments/environment';
+import { ProductoService } from 'src/app/servicios/producto.service';
+import { MarcaService } from 'src/app/servicios/marca.service';
+import { CategoriaService } from 'src/app/servicios/categoria.service';
+import { TipoPesoService } from 'src/app/servicios/tipo-peso.service';
+import { nextTick } from 'process';
 
 @Component({
   selector: 'app-ingreso-producto',
@@ -10,15 +15,16 @@ import { Router } from '@angular/router'
 })
 export class IngresoProductoComponent implements OnInit {
 
-  
+  button:boolean=true;
+  tipo:any = [];
+  animals:any = [];
+
   public _form !: FormGroup
+  private _formB!:FormBuilder
 
-  constructor(
+    constructor(private api:ProductoService, private router:Router) {
 
-    private _formB:FormBuilder,
-    private router:Router,
-   
-    ) { }
+    }
 
   ngOnInit(): void {
     this._form=this._formB.group({
@@ -33,42 +39,59 @@ export class IngresoProductoComponent implements OnInit {
     })
   }
 
+  
 store(_form:any)
 {
-   if (this._form.valid) {
-    console.log("formulario", _form);
-    
-   } else {
+ 
+  this.api.Registrarproducto(_form).subscribe({
+  next:(res)=>(console.log("hola")),
+})
+
+  if(this._form){
+   
+  } else {
+
+    this._form.markAllAsTouched()
    
    }
-  
+}
    
-  }
   
-  /* succes(){
-      swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'Registrado con exito',
-        showConfirmButton: false,
-         timer: 1500
-      }
-      )
 
-  } 
-  error(){
-    swal.fire({
-      position: 'top-end',
-      icon: 'error',
-      title: 'Asegurese de llenar todos los datos',
-      showConfirmButton: false,
-       timer: 1500
-    }
-    )
+
+  registrar(_form:any){
+    this.api.Registrarproducto(_form).subscribe(data=>(console.log(data)));
+    
   }
 
-  cambio(){
-    this.router.navigate(['notas'])
-  } */
+  editar(_id:any){
+    localStorage.setItem("id",_id);
+    this.button=false
+   this.api.Editarproducto(_id).subscribe(data=>{ 
+      let t:any=data;
+      this._form.setValue({
+      'nombre':t.nombre,
+      'precio':t.precio,
+      'peso':t.peso,
+      'stock':t.stock,
+      'id_categoria':t.id_categoria,
+      'id_marca':t.id_marca,
+      'id_tipo_peso':t.id_tipo_peso,
+      'imagen':t.imagen
+      });
+      (console.log(data))
+    });
+  }
+
+  borrar(_form:any){
+    let _id=localStorage.getItem("id")
+    this.api.Borrarproducto(_form,_id).subscribe(data=>(console.log(data)));
+  }
+
+ eliminar(_id:any){
+    this.api.Eliminarproducto(_id).subscribe(data=>(console.log(data)));
+  }
+
+
   
 }
--- src/app/servicios/producto.service.ts
+++ src/app/servicios/producto.service.ts
@@ -1,29 +1,69 @@
 import { Injectable } from '@angular/core';
-import {HttpClient,HttpParams} from '@angular/common/http'
+import {HttpClient,HttpParams, HttpHeaders} from '@angular/common/http'
 import { environment } from 'src/environments/environment';
+import { Observable, observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
+
   constructor(private http:HttpClient) { }
 
-  guardar()
+  getUsuarios(){
+    return this.http.get<any>(`${environment.urlApi}producto`)
+  }
+
+  // public _form = new FormGroup({
+  //   nombre: new FormControl('',Validators.required),
+  //   precio: new FormControl('',Validators.required),
+  //   peso: new FormControl('',Validators.required),
+  //   stock: new FormControl('',Validators.required),
+  //   imagen: new FormControl('',Validators.required),
+  //   id_categoria: new FormControl('',Validators.required),
+  //   id_marca: new FormControl('',Validators.required),
+  //   id_tipo_peso: new FormControl('',Validators.required),
+
+  
+  // })
+
+
+  Registrarproducto(form:any){
+    debugger
+    return this.http.post<any>(`${environment.urlApi}producto`,form)
+  }
+
+  Guardarproducto()
   {
-    const header=new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')}`);
-    let dir= this.url+"guardar";
+    const header=new HttpHeaders()
+    .set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    let dir= this.http+"producto";
     return this.http.get<any>(dir, {headers:header});
   }
 
-  Storeproducto(form:any){
-    const params = new FormData();
-    params.set('nombre',form.nombre);
-    params.set('precio',form.precio);
-    params.set('peso',form.peso);
-    params.set('stock',form.stock);
-    params.set('imagen',form.imagen);
-
-    return this.http.post<any>(`${environment.urlApi}register`,params)
+  Editarproducto(_id:any):Observable<any>
+  {
+    const header=new HttpHeaders()
+    .set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    let dir= this.http+"producto/"+_id;
+    return this.http.get<any>(dir,{headers:header});
   }
+  Borrarproducto(_form:any,_id:any)
+  {
+    const header=new HttpHeaders()
+    .set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    let dir= this.http+"producto/"+_id;
+    return this.http.post<any>(dir,_form,{headers:header});
+  }
+
+  Eliminarproducto(_id:any)
+  {
+    const header=new HttpHeaders()
+    .set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    let dir= this.http+"producto/"+_id;
+   return this.http.post<any>(dir,_id,{headers:header});
+  }
+
+
 }
